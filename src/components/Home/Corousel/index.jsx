2e7ce627a94710cad086f7311d7bceebe4b7a5d8
abdulvokhid1 <ibrakhimov7747a@gmail.com>
2022-05-23 16:14:14 +0900

    done

--- src/components/Home/Corousel/index.jsx
+++ src/components/Home/Corousel/index.jsx
@@ -1,16 +1,21 @@
-import React from "react";
-import { Container, Img } from "./style";
+import React, { useRef } from "react";
+import { Container, Icon, Img, Wrapper } from "./style";
 import img1 from "../../../assets/imgs/img1.png";
 import img2 from "../../../assets/imgs/img2.png";
 
 export const Home = () => {
+  const slider = useRef();
   return (
-    <Container dots autoplay>
-      <Img src={img2} />
-      <Img src={img1} />
-      <Img src={img2} />
-      <Img src={img1} />
-    </Container>
+    <Wrapper>
+      <Icon.Left onClick={() => slider.current.prev()} />
+      <Icon.Right onClick={() => slider.current.next()} />
+      <Container ref={slider} dots autoplay>
+        <Img src={img2} />
+        <Img src={img1} />
+        <Img src={img2} />
+        <Img src={img1} />
+      </Container>
+    </Wrapper>
   );
 };
 export default Home;
--- src/components/Home/Corousel/style.js
+++ src/components/Home/Corousel/style.js
@@ -1,10 +1,17 @@
 import styled from "styled-components";
 import { Carousel } from "antd";
-import { ReactComponent as arrowleft } from "../../../assets/icons/arrowleft";
+import { ReactComponent as arrowleft } from "../../../assets/icons/arrowleft.svg";
+import { ReactComponent as arrowright } from "../../../assets/icons/arrowright.svg";
+
+const Wrapper = styled.div`
+  position: relative;
+`;
+
 const Container = styled(Carousel)`
   height: 570px;
   width: 100%;
 `;
+
 const Img = styled.img`
   background: black;
   width: 100%;
@@ -19,6 +26,45 @@ Icon.Left = styled(arrowleft)`
   position: absolute;
   top: 50%;
   left: 0;
-  transform: translate(100%, -50%);
+  transform: translate(100%, -100%);
+  width: 45px;
+  height: 45px;
+  color: white;
+  background: white;
+  padding: 12px;
+  opacity: 0.5;
+  border-radius: 50%;
+  z-index: 999;
+  cursor: pointer;
+  :hover {
+    opacity: 0.97;
+  }
+  & path {
+    fill: #0d263b;
+  }
 `;
-export { Container, Img, Icon };
+Icon.Right = styled(arrowleft)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 50%;
+  right: 0;
+  transform: translate(-100%, -100%) rotate(180deg);
+  width: 45px;
+  height: 45px;
+  color: black;
+  background: white;
+  padding: 12px;
+  opacity: 0.5;
+  border-radius: 50%;
+  z-index: 999;
+  cursor: pointer;
+  :hover {
+    opacity: 0.97;
+  }
+  & path {
+    fill: #0d263b;
+  }
+`;
+export { Container, Img, Icon, Wrapper };
