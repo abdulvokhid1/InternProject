49b9be786c05b37dd1b4e2eedd1801bd58fd37f6
abdulvokhid1 <ibrakhimov7747a@gmail.com>
2022-06-03 16:22:30 +0900

    done

--- src/pages/Signin/index.jsx
+++ src/pages/Signin/index.jsx
@@ -2,14 +2,44 @@ import React, { useState } from "react";
 import { Container, Wrapper } from "./style";
 import Input from "../../components/Generic/Input";
 import { Button } from "../../components/Generic/Button";
+import { useQuery, useMutation } from "react-query";
+import { useNavigate } from "react-router-dom";
+
+const { REACT_APP_BASE_URL: url } = process.env;
 
 export const Generic = () => {
   const [email, setEmail] = useState("");
   const [pw, setPw] = useState("");
 
+  const navigate = useNavigate();
+
+  const { mutate } = useMutation(
+    () => {
+      fetch(`${url}/public/auth/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password: pw }),
+      }).then((res) => res.json());
+    },
+    {
+      onSuccess: (res) => {
+        console.log(res, "res");
+        localStorage.setItem("token", res?.authenticationToken);
+        console.log(res?.authenticationToken);
+        if (res?.authenticationToken) navigate("/home");
+      },
+      onError: (res) => {
+        console.log(res, "error");
+      },
+    }
+  );
+
   const onSubmit = () => {
     console.log(email);
     console.log(pw);
+    mutate();
   };
   return (
     <Container>
